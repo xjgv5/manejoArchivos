9842bde1f3feaa376158bf6a2852bcb2871684cd
Javier Gomez <javier.gov@outlook.es>
2023-12-18 23:49:32 -0600

    Creacion y escritura con FileWriter

--- src/com/xjgv/archivos/ejemplos/EjemploCrearArchivo.java
+++ src/com/xjgv/archivos/ejemplos/EjemploCrearArchivo.java
@@ -1,7 +1,12 @@
 package com.xjgv.archivos.ejemplos;
 
+import com.xjgv.archivos.ejemplos.service.ArchivoServicio;
+
 public class EjemploCrearArchivo {
     public static void main(String[] args) {
+        String nombreArchivo = "D:\\Java Projects\\manejoArchivos\\archivos\\java.txt";
 
+        ArchivoServicio servicio = new ArchivoServicio();
+        servicio.crearArchivo(nombreArchivo);
     }
 }
--- src/com/xjgv/archivos/ejemplos/service/ArchivoServicio.java
+++ src/com/xjgv/archivos/ejemplos/service/ArchivoServicio.java
@@ -8,7 +8,12 @@ public class ArchivoServicio {
     public void crearArchivo(String nombre){
         File archivo = new File(nombre);
         try {
-            FileWriter escritor = new FileWriter(archivo);
+            FileWriter escritor = new FileWriter(archivo, true);
+            escritor.append("Prueba de escritura")
+                    .append("\n")
+                    .append("Prueba terminada");
+            escritor.close();
+            System.out.println("El archivo se ha creado con exito! ");
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
